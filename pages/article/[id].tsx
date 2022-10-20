7de7e54bd1e408d0cf5838a0092533c109b1be9e
Vladimir <vovalol31@gmail.com>
2022-10-20 14:35:50 +0300

    1231232323

--- pages/article/[id].tsx
+++ pages/article/[id].tsx
@@ -1,16 +1,17 @@
 export default function ({ article }) {
+	console.log(article)
 	const { id } = article
 	return (
 		<>
-			<div>id:{ article.id }</div>
-			<div>{ article.title }</div>
-			<div>{ article.content[0] }</div>
+			<div>id: { article.id }</div>
+			<div>title: { article.title }</div>
+			<div>content: { article.content[0] }</div>
 		</>
 	)
 }
 
 export async function getServerSideProps(context) {
-	const response = await fetch(`http://tomsk-news.ru:5000/api/article/${ context.query.id }`)
+	const response = await fetch(`http://tomsk-news.ru:5000/api/article/?id=${ context.query.id }`)
 	const article = await response.json()
 	return {
 		props: { article },
